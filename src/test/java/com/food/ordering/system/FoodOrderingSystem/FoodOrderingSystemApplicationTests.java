f58c6db79b2b15f554ae12bd3af6764ab81927ab
VamshiKumar <128585565+KVamshiReddy@users.noreply.github.com>
2024-04-12 09:53:39 +0530

    Initial commit

---
+++ src/test/java/com/food/ordering/system/FoodOrderingSystem/FoodOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.FoodOrderingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
